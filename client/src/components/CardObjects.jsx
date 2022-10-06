fbbbf8a8b77fd428464cb94738c8a13aff4a9270
Roman Home <t89875041999@yandex.ru>
2022-10-06 22:00:17 +0300

    Changes to be committed: 	modified:   client/src/components/CardObjects.jsx
     Changes not staged for commit:
    	modified:   client/src/features/objectBuild/objectBuildSlice.js
    	modified:   client/src/modals/AddObject.jsx
    
     Untracked files:
    	client/src/pages/OneObject.jsx
    	client/src/routes.js
    	client/src/utils/

--- client/src/components/CardObjects.jsx
+++ client/src/components/CardObjects.jsx
@@ -1,8 +1,26 @@
 import React from 'react';
 import {Button, Card} from "react-bootstrap";
 import { BsFillXCircleFill } from 'react-icons/bs';
-const CardObjects = ({name, address}) => {
+import { useDispatch } from 'react-redux';
+import { removeObject } from '../features/objectBuild/objectBuildSlice';
+import { useNavigate } from "react-router-dom"
 
+const CardObjects = ({id, name, address}) => {
+
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+
+    const removeObjectHandler = (id) => {
+        dispatch(removeObject(id))
+    } 
+
+    const question = (id) => {
+        
+        const isDel = window.confirm(`Точно удаляем ${name}?`)
+        if (isDel) {
+            removeObjectHandler(id)
+        }
+    }
 
     return (
         <Card className="mt-3" style={{ width: '15rem', margin: "10px 10px 0 0" }}>
@@ -12,8 +30,12 @@ const CardObjects = ({name, address}) => {
                 <Card.Text>
                     {address}
                 </Card.Text>
-                <Button variant="primary">Вперёд</Button>
-                < BsFillXCircleFill color={"red"} style={{marginLeft: "2rem", cursor: "pointer"}}/>
+                <Button variant="primary" onClick={() => navigate()}>Вперёд</Button>
+                < BsFillXCircleFill 
+                    onClick={() => question(id)}
+                color={"red"} 
+                style={{marginLeft: "2rem", cursor: "pointer"}}
+                />
             </Card.Body>
         </Card>
     );
