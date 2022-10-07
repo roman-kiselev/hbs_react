4cf7f594868d108ecf37c97753bd15c3e770cbda
Roman <roman@roman.com>
2022-10-07 15:36:40 +0300

    Changes to be committed: 	modified:   client/package-lock.json 	modified:   client/package.json 	modified:   client/src/pages/HomePage.jsx 	modified:   client/src/pages/OneObject.jsx

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import { Container, Row, Button, Col } from "react-bootstrap"
+import {useNavigate} from "react-router-dom"
+import { OBJECT_BUILD_ROUTE } from '../utils/consts'
 
 const HomePage = () => {
+
+  const navigate = useNavigate()
+
   return (
-    <div>HomePage</div>
+    <Container style={{display: "flex"}}>
+      <Row className="mx-auto" style={{display: "flex", justifyContent: "center", marginTop: "25%"}}>
+        <Col style={{display: "flex"}}>
+          <Button variant="primary" onClick={() => navigate(OBJECT_BUILD_ROUTE)}>Объекты</Button>
+        </Col>
+        <Col>
+          <Button variant="secondary">Администрирование</Button>
+        </Col>
+        <Col>
+          <Button variant="success">Что то</Button>
+        </Col>
+        <Col>
+          <Button variant="warning">Что то</Button>
+        </Col>
+        <Col>
+          <Button variant="danger">Что то</Button>
+        </Col>
+        
+      </Row>
+    </Container>
   )
 }
 
