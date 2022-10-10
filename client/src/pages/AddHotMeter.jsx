33b27982f694dec1af7bc00adece4fc94a55b45b
Roman <roman@roman.com>
2022-10-10 16:40:32 +0300

    Changes to be committed: 	modified:   client/src/pages/AddHotMeter.jsx

--- client/src/pages/AddHotMeter.jsx
+++ client/src/pages/AddHotMeter.jsx
@@ -1,10 +1,38 @@
 import React from 'react'
-import { TreeViewComponent } from "@syncfusion/ej2-react-navigations";
+
+import { Container, Row, Col } from 'react-bootstrap';
 
 
 const AddHotMeter = () => {
   return (
-    
+    <Container fluid>
+        <Row>
+            <Col md={2} style={{borderRight: "1px solid grey", overflow: "scroll"}}>
+                <Row>
+                    overflow: scroll
+                    overflow: scroll
+                    overflow: scroll
+                    overflow: scroll
+                    overflow: scroll
+                    overflow: scroll
+                    overflow: scroll
+                    overflow: scroll
+                    overflow: scroll
+                    overflow: scroll
+                    overflow: scroll
+                    overflow: scroll
+                    overflow: scroll
+                    overflow: scroll
+                </Row>
+            </Col>
+
+            <Col>
+            asdasd
+            
+            
+            </Col>
+        </Row>
+    </Container>
   )
 }
 
