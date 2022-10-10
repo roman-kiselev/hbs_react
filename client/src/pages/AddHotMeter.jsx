2a5467c1f95e023c608785bff42f00c514cd360a
Roman <roman@roman.com>
2022-10-10 16:06:53 +0300

    Changes to be committed: 	new file:   client/src/pages/AddHotMeter.jsx
     Changes not staged for commit:
    	modified:   client/package-lock.json
    	modified:   client/package.json
    	modified:   client/src/pages/OneObject.jsx
    	modified:   client/src/routes.js
    	modified:   client/src/utils/consts.js

---
+++ client/src/pages/AddHotMeter.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { TreeViewComponent } from "@syncfusion/ej2-react-navigations";
+
+
+const AddHotMeter = () => {
+  return (
+    
+  )
+}
+
+export default AddHotMeter;
