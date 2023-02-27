a3a7f2a130632bf66638d32e8957af52278a3863
RomanLaptop <t89875041999@yandex.ru>
2023-02-27 22:28:07 +0300

    Changes to be committed: 	new file:   client/src/components/addMeters/bolid/formMeter/FormCoolHotMeterBolid.jsx 	new file:   client/src/modals/settings/AddFlat.jsx 	new file:   client/src/modals/settings/AddFloors.jsx 	new file:   client/src/modals/settings/AddLine.jsx 	new file:   client/src/modals/settings/AddOffice.jsx 	new file:   client/src/modals/settings/AddSections.jsx
     Changes not staged for commit:
    	modified:   client/.env
    	modified:   client/src/components/addMeters/bolid/AddCoolMetersBolid.jsx
    	modified:   client/src/components/addMeters/bolid/AddMeterCoolHotBolid.jsx
    	modified:   client/src/components/addMeters/bolid/formMeter/CitiesInput.jsx
    	modified:   client/src/components/addMeters/bolid/formMeter/FormCoolHotMeterBolid.jsx
    	modified:   client/src/components/addMeters/bolid/formMeter/FormCoolMeterBolid.jsx
    	modified:   client/src/components/objects/ListObjects.jsx
    	modified:   client/src/components/settings/SettingsBolid.jsx
    	modified:   client/src/features/objectBuild/objectBuildSlice.js
    	modified:   client/src/modals/AddObject.jsx
    	modified:   client/src/modals/settings/AddFlat.jsx
    	modified:   client/src/modals/settings/AddFloors.jsx
    	modified:   client/src/modals/settings/AddLine.jsx
    	modified:   client/src/modals/settings/AddOffice.jsx
    	modified:   client/src/modals/settings/AddSections.jsx
    	modified:   server/db.js
    	modified:   server/index.js
    	modified:   server/models/models.js
    	modified:   server/routes/index.js
    
     Untracked files:
    	server/controller/objects/FlatsController.js
    	server/controller/objects/FloorsController.js
    	server/controller/objects/LineRouter.js
    	server/controller/objects/OfficeController.js
    	server/controller/objects/SectionsController.js
    	server/routes/objects/flatsRouter.js
    	server/routes/objects/floorsRouter.js
    	server/routes/objects/lineRoutes.js
    	server/routes/objects/officeRouter.js
    	server/routes/objects/sectionsRouter.js
    	server/static/objects/img/03bbdc03-2d3e-4480-a25c-3ec9ad668efe.jpg
    	server/static/objects/img/1d305164-3a74-474b-9fe4-9cffe9126393.jpg
    	server/static/objects/img/6fe324ef-bc5c-4b6a-ad0e-f6d8bc1d7839.jpg
    	server/static/objects/img/915bef50-6ec1-4d17-9ab8-ae7f6a15579b.jpg
    	server/static/objects/img/ab80e538-f862-4301-a2ba-b49bde520c93.jpg
    	server/static/objects/img/b6600d3d-69de-44d7-a336-955d9299f344.jpg
    	server/static/objects/img/f7816a37-154a-41d4-bb81-65dbafb9e1c7.jpg
    	server/static/objects/img/fc156ea9-4380-4535-aa5a-a8e6983ed189.jpg

---
+++ client/src/modals/settings/AddOffice.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import {Button, Form, Modal} from "react-bootstrap";
+
+const AddFlat = ({show, onHide}) => {
+
+    return (
+        <Modal show={show} onHide={onHide}>
+            <Modal.Header closeButton>
+                <Modal.Title>Добавить квартиры</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <Form>
+                    <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control
+                            type="email"
+                            placeholder="name@example.com"
+                            autoFocus
+                        />
+                    </Form.Group>
+                    <Form.Group
+                        className="mb-3"
+                        controlId="exampleForm.ControlTextarea1"
+                    >
+                        <Form.Label>Example textarea</Form.Label>
+                        <Form.Control as="textarea" rows={3} />
+                    </Form.Group>
+                </Form>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button variant="secondary" onClick={onHide}>
+                    Close
+                </Button>
+                <Button variant="primary" onClick={onHide}>
+                    Save Changes
+                </Button>
+            </Modal.Footer>
+        </Modal>
+    );
+};
+
+export default AddFlat;
