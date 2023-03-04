40d5a9237d4b945b758d62215f7ee79919224fd5
Roman <t89875041999@yandex.ru>
2023-03-04 11:21:44 +0300

    Changes to be committed: 	modified:   client/src/components/pagination/Pages.jsx

--- client/src/components/pagination/Pages.jsx
+++ client/src/components/pagination/Pages.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import {Pagination} from "react-bootstrap";
-import {useDispatch, useSelector} from "react-redux";
-import {setCurrentPage} from "../../features/testMeters/testWaterMeterSlice";
+import { Pagination } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { setCurrentPage } from "../../features/testMeters/testWaterMeterSlice";
 
-const Pages = ({totalCount, limit, currentPage}) => {
+const Pages = ({ totalCount, limit, currentPage }) => {
     const dispatch = useDispatch()
     // const {totalCount} = useSelector((state) => state.mainTable)
     // const {limit} = useSelector((state) => state.mainTable)
@@ -11,10 +11,29 @@ const Pages = ({totalCount, limit, currentPage}) => {
     const pageCount = Math.ceil(totalCount / limit)
     const pages = []
 
-
-    for (let i = 0; i < pageCount; i++) {
-        pages.push(i + 1)
+    if(pageCount > 10) {
+        if(currentPage > 5) {
+            for (let i = currentPage-4; i <= currentPage+5; i++) {
+                pages.push(i)
+                if(i == pageCount) break
+            }
+        }
+        else {
+            for (let i = 1; i <= 10; i++) {
+                pages.push(i)
+                if(i == pageCount) break
+            }
+        }
+    }  else {
+        for (let i = 1; i <= pageCount; i++) {
+            pages.push(i)
+        }
     }
+
+    // for (let i = 0; i < pageCount; i++) {
+    
+    //     pages.push(i + 1)
+    // }
 
     return (
         <Pagination>
