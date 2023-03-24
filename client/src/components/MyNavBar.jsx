fea02d007fa43f4ea815cc480ca4a9798ee856da
Roman <t89875041999@yandex.ru>
2023-03-24 11:27:48 +0300

    Changes to be committed: 	modified:   client/src/components/MyNavBar.jsx

--- client/src/components/MyNavBar.jsx
+++ client/src/components/MyNavBar.jsx
@@ -1,8 +1,27 @@
-import React from 'react';
-import {Nav, Navbar, NavDropdown, Container, Form, Button} from "react-bootstrap";
-import {NavLink} from "react-router-dom";
+import React from "react";
+import {
+    Nav,
+    Navbar,
+    NavDropdown,
+    Container,
+    Form,
+    Button,
+} from "react-bootstrap";
+import { useDispatch } from "react-redux";
+import { NavLink, useNavigate } from "react-router-dom";
+import { setIsAuth, setUser } from "../features/user/userSlice";
 
 const MyNavBar = () => {
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+
+    const handleExit = () => {
+        navigate("/");
+        localStorage.removeItem("token");
+        dispatch(setUser({}));
+        dispatch(setIsAuth(false));
+    };
+
     return (
         <Navbar bg="light" expand="lg">
             <Container fluid>
@@ -11,16 +30,36 @@ const MyNavBar = () => {
                 <Navbar.Collapse id="navbarScroll">
                     <Nav
                         className="me-auto my-2 my-lg-0"
-                        style={{ maxHeight: '100px' }}
+                        style={{ maxHeight: "100px" }}
                         navbarScroll
                     >
                         {/*<NavLink style={{textDecoration: "none"}} to="/"><Nav.Link href="/">Домашняя</Nav.Link></NavLink>*/}
-                        <NavLink style={{textDecoration: "none", margin: 7, color: "grey"}} to="/">Домашняя</NavLink>
-                        <NavLink style={{textDecoration: "none", margin: 7, color: "grey"}} to="/admin">Админ панель</NavLink>
-                       {/* <NavLink style={{textDecoration: "none"}} to="/"><Nav.Link href="#action2">Link</Nav.Link></NavLink>*/}
+                        <NavLink
+                            style={{
+                                textDecoration: "none",
+                                margin: 7,
+                                color: "grey",
+                            }}
+                            to="/"
+                        >
+                            Домашняя
+                        </NavLink>
+                        <NavLink
+                            style={{
+                                textDecoration: "none",
+                                margin: 7,
+                                color: "grey",
+                            }}
+                            to="/admin"
+                        >
+                            Админ панель
+                        </NavLink>
+                        {/* <NavLink style={{textDecoration: "none"}} to="/"><Nav.Link href="#action2">Link</Nav.Link></NavLink>*/}
 
                         <NavDropdown title="Пусто" id="navbarScrollingDropdown">
-                            <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+                            <NavDropdown.Item href="#action3">
+                                Action
+                            </NavDropdown.Item>
                             <NavDropdown.Item href="#action4">
                                 Another action
                             </NavDropdown.Item>
@@ -32,6 +71,23 @@ const MyNavBar = () => {
                         <Nav.Link href="#" disabled>
                             Пусто
                         </Nav.Link>
+                        <Nav className="d-flex">
+                            <NavDropdown
+                                title="Профиль"
+                                id="navbarScrollingDropdown"
+                            >
+                                <NavDropdown.Item href="#action3">
+                                    Action
+                                </NavDropdown.Item>
+                                <NavDropdown.Item href="#action4">
+                                    Another action
+                                </NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item href="/" onClick={handleExit}>
+                                    Выход
+                                </NavDropdown.Item>
+                            </NavDropdown>
+                        </Nav>
                     </Nav>
                     <Form className="d-flex">
                         <Form.Control
