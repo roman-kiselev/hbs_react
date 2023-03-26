b6d0b1edfb28408126dcd2dfc7caef88b08bd70f
RomanLaptop <t89875041999@yandex.ru>
2023-03-26 22:54:06 +0300

    Changes to be committed: 	modified:   client/src/components/kdlInObject/view/MainKdlInObject.jsx

--- client/src/components/kdlInObject/view/MainKdlInObject.jsx
+++ client/src/components/kdlInObject/view/MainKdlInObject.jsx
@@ -1,10 +1,51 @@
 import React from "react";
+import { Col, ListGroup, Row } from "react-bootstrap";
+import { BsCircle, BsCircleFill } from "react-icons/bs";
 
 const MainKdlInObject = () => {
+    const [kdl, setKdl] = React.useState([
+        {
+            name: "КДЛ №1",
+        },
+        {
+            name: "КДЛ №2",
+        },
+        {
+            name: "КДЛ №3",
+        },
+    ]);
+
+    const handleClick = () => {};
+
     return (
-        <div>
-            <h1>КДЛ объекта</h1>
-        </div>
+        <Row>
+            <Row>
+                <h5>Кдл к объекту</h5>
+            </Row>
+            <Row className="mt-3">
+                <Row className="mt-3">
+                    <Col sm={4}>
+                        <ListGroup>
+                            {kdl.map((item, index) => (
+                                <ListGroup.Item
+                                    className="mb-1 mt-1"
+                                    style={{ cursor: "pointer" }}
+                                    key={index}
+                                >
+                                    {item.name}
+                                </ListGroup.Item>
+                            ))}
+                        </ListGroup>
+                    </Col>
+
+                    <Col sm={8}>
+                        <BsCircle>1</BsCircle>
+
+                        <BsCircleFill>49</BsCircleFill>
+                    </Col>
+                </Row>
+            </Row>
+        </Row>
     );
 };
 
