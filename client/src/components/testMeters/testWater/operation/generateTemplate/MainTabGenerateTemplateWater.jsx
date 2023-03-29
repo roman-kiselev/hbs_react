42f71521f62961b82a11f44253f8109ab1dc3640
Roman <t89875041999@yandex.ru>
2023-03-29 16:18:36 +0300

    Changes to be committed: 	modified:   client/src/components/testMeters/testWater/operation/generateTemplate/MainTabGenerateTemplateWater.jsx
     Changes not staged for commit:
    	modified:   client/src/components/AppRouter.jsx
    	modified:   server/controller/mainTable/devices/DeviceKdlController.js
    	modified:   server/controller/mainTable/objects/MainTableSectionController.js
    	modified:   server/routes/mainTable/devices/DeviceKdlRouter.js
    	modified:   server/routes/mainTable/objects/mainTableSectionRouter.js
    
     Untracked files:
    	client/src/http/mainTableApi/

--- client/src/components/testMeters/testWater/operation/generateTemplate/MainTabGenerateTemplateWater.jsx
+++ client/src/components/testMeters/testWater/operation/generateTemplate/MainTabGenerateTemplateWater.jsx
@@ -1,26 +1,72 @@
 import React from "react";
 import { Button, Col, Form, Row } from "react-bootstrap";
 import { RiFileExcel2Line } from "react-icons/ri";
+import { useQuery } from "react-query";
+import { getAllSections } from "../../../../../http/mainTableApi/objects/sectionsApi";
 
 const MainTabGenerateTemplateWater = ({ objectBuildId }) => {
+    // Состояние selected
+    const [selectedSection, setSelectedSection] = React.useState("");
+    const [selectedKdl, setSelectedKdl] = React.useState("");
+    // Изменяем состояние
+    const handleChangeSection = (e) => {
+        setSelectedSection(e.target.value);
+    };
+    const handleChangeKdl = (e) => {
+        setSelectedKdl(e.target.value);
+        //
+    };
+    // Список всех секций
+    const [sections, setSections] = React.useState([]);
+    // Список всех КДЛ
+    const [kdl, setKdl] = React.useState([]);
+
+    // Используем useQuery для получения данных из БД
+    const { isLoading, error, data } = useQuery("sections", async () => {
+        const { sections } = await getAllSections(objectBuildId);
+        setSections(sections);
+    });
+
+    // Проверка пользователей
+    if (isLoading) {
+        return <div>Loading...</div>;
+    }
+    if (error) {
+        return <div>Error: {error.message}</div>;
+    }
+
     return (
         <Row>
             <Row>
                 <Col sm={3}>
-                    <Form.Select aria-label="Default select example">
-                        <option>Выбор секции</option>
-                        <option value="1">1</option>
-                        <option value="2">2</option>
-                        <option value="3">3</option>
+                    <Form.Select
+                        value={selectedSection}
+                        onChange={handleChangeSection}
+                        aria-label="Default select example"
+                    >
+                        {sections.map((section, i) => (
+                            <option key={i} value={section.section}>
+                                {section.section}
+                            </option>
+                        ))}
                     </Form.Select>
                 </Col>
 
                 <Col sm={3}>
-                    <Form.Select aria-label="Default select example">
-                        <option>Выбор КДЛ</option>
-                        <option value="1">1</option>
-                        <option value="2">2</option>
-                        <option value="3">3</option>
+                    <Form.Select
+                        value={selectedKdl}
+                        onChange={handleChangeKdl}
+                        aria-label="Default select example"
+                    >
+                        {selectedSection === "" ? (
+                            <option>Выберите секцию</option>
+                        ) : (
+                            kdl.map((k, i) => (
+                                <option key={i} value={k.listKdl}>
+                                    {k.listKdl}
+                                </option>
+                            ))
+                        )}
                     </Form.Select>
                 </Col>
                 <Col>
