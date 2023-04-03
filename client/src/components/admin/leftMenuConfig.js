9bc3c8e973ed15871b296e916a88bd44f9567806
Roman <t89875041999@yandex.ru>
2023-04-03 16:25:55 +0300

    Changes to be committed: 	modified:   client/src/components/admin/leftMenuConfig.js 	new file:   server/controller/admin/BrandsController.js 	new file:   server/controller/admin/DeviceController.js 	new file:   server/controller/admin/MetersController.js 	new file:   server/routes/admin/brandsRouter.js 	new file:   server/routes/admin/metersRouter.js 	new file:   server/routes/admin/meviceRouter.js

--- client/src/components/admin/leftMenuConfig.js
+++ client/src/components/admin/leftMenuConfig.js
@@ -1,8 +1,26 @@
 export const leftMenuConfig = [
-    {
-        toItem: `users`,
-        action: true,
-        variant: "light",
-        title: "Пользователи",
-    },
+  {
+    toItem: `users`,
+    action: true,
+    variant: "light",
+    title: "Пользователи",
+  },
+  {
+    toItem: `brends`,
+    action: true,
+    variant: "light",
+    title: "Бренды",
+  },
+  {
+    toItem: `createMeters`,
+    action: true,
+    variant: "light",
+    title: "Создать счётчики",
+  },
+  {
+    toItem: `createDevice`,
+    action: true,
+    variant: "light",
+    title: "Создать устройство",
+  },
 ];
