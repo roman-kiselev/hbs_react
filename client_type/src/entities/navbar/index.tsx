6c1ba6f430576d9221d6f80f9c34472970e69611
Roman <t89875041999@yandex.ru>
2023-04-19 14:22:55 +0300

    Changes to be committed: 	modified:   client_type/src/entities/navbar/index.tsx 	modified:   client_type/src/entities/navbar/ui/NavAskue.tsx

--- client_type/src/entities/navbar/index.tsx
+++ client_type/src/entities/navbar/index.tsx
@@ -1,11 +1,18 @@
 import React from "react";
+import { Container, Navbar } from "react-bootstrap";
 import { configNav } from "../../shared/config";
 import { NavAskue } from "./ui";
 const NavbarAskue: React.FC = () => {
     return (
-        <>
-            <NavAskue configData={configNav} />
-        </>
+        <Navbar bg="light" expand="lg">
+            <Container fluid>
+                <Navbar.Brand href="#">АСКУЭ</Navbar.Brand>
+                <Navbar.Toggle aria-controls="navbarScroll" />
+                <Navbar.Collapse id="navbarScroll">
+                    <NavAskue configData={configNav} />
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
 };
 
--- client_type/src/entities/navbar/ui/NavAskue.tsx
+++ client_type/src/entities/navbar/ui/NavAskue.tsx
@@ -1,15 +1,14 @@
 import React from "react";
-import { Nav } from "react-bootstrap";
 import NavLinkAskue from "./NavLinkAskue";
 import { IConfig } from "../../../shared/interfaces";
 
 const NavAskue: React.FC<IConfig> = ({ configData }) => {
     return (
-        <h6>
+        <>
             {configData.map((item) => (
                 <NavLinkAskue key={item.to} {...item} />
             ))}
-        </h6>
+        </>
     );
 };
 
