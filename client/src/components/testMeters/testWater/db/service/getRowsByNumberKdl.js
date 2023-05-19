33348c18736b2a493247b08263326b2260c144f7
Roman <t89875041999@yandex.ru>
2023-05-19 13:09:50 +0300

    Changes to be committed: 	modified:   client/src/components/testMeters/testWater/db/service/getRowsByNumberKdl.js

--- client/src/components/testMeters/testWater/db/service/getRowsByNumberKdl.js
+++ client/src/components/testMeters/testWater/db/service/getRowsByNumberKdl.js
@@ -1,7 +1,11 @@
 import db from "../db";
 
 async function getRowsByNumberKdl(numberKdl) {
-    const rows = await db.main.where("numberKdl").equals(numberKdl).toArray();
+    const rows = await db.main
+        .where("numberKdl")
+        .equals(numberKdl)
+        .sortBy("numberAsr");
+
     return rows;
 }
 
