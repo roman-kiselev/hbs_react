ac9e4e3ed99a514ea4c2a8707e3a86647659d698
RomanLaptop <t89875041999@yandex.ru>
2023-06-15 22:41:48 +0300

    Changes to be committed: 	new file:   server_type/src/services/mainTable/main-water.service.ts
     Changes not staged for commit:
    	modified:   server_type/src/controllers/objects/objects-build.controller.ts
    	modified:   server_type/src/dto/createDto/index.ts
    	modified:   server_type/src/dto/index.ts
    	modified:   server_type/src/lib/index.ts
    	modified:   server_type/src/services/files/file.service.ts
    	modified:   server_type/src/services/objects/floors.service.ts
    	modified:   server_type/src/services/objects/object_builds.service.ts
    	modified:   server_type/src/services/objects/sections.service.ts
    	modified:   server_type/src/services/users/role.service.ts
    	modified:   server_type/src/services/users/users.service.ts
    
     Untracked files:
    	server_type/src/dto/createDto/mainTable/
    	server_type/src/lib/mainTable/
    	server_type/src/services/mainTable/index.ts
    	server_type/src/services/mainTable/main-table.service.ts

---
+++ server_type/src/services/mainTable/main-water.service.ts
@@ -0,0 +1,12 @@
+import { CreateMainWaterDto } from "../../dto";
+
+class MainWater {
+    async addNewWaterMeter(dto: CreateMainWaterDto) {
+        try {
+        } catch (e) {
+            console.error(e);
+        }
+    }
+}
+
+export default new MainWater();
