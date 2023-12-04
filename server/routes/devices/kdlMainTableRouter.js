e0df1105ad8b9722d2992ab63e35cece03e51642
RomanJob <t89875041999@yandex.ru>
2023-12-04 15:50:36 +0300

    modified:   server/routes/devices/kdlMainTableRouter.js

--- server/routes/devices/kdlMainTableRouter.js
+++ server/routes/devices/kdlMainTableRouter.js
@@ -1,7 +1,6 @@
 import express from "express";
-const router = express.Router();
 import KdlMainTableController from "../../controller/devices/kdlMainTableController.js";
-import authMiddleware from "../../middleware/authMiddleware.js";
+const router = express.Router();
 
 // Получаем все записи из базы данных без повторов
 router.get("/", KdlMainTableController.getAllUnique);
